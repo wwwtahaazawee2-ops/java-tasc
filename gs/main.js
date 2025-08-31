5fe8e2e66f158437a44ff330964166b2b9a91a50
wwwtahaazawee2-ops <wwwtahaazawee2@gmail.com>
2025-08-31 16:19:33 +0300

    h1

---
+++ gs/main.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+/*  var nedded=3;
+function passed(mark){
+    if(mark>=60)
+    {
+      return true;
+    }
+    else if(mark + nedded>=60)
+        {
+nedded=nedded-(60-mark);
+return true;
+    }
+}  */
+/* const arr = [5,8,9,7];
+console.log(arr.length)
+for(let x=0; x<arr.length ; x++)
+    {
+console.log(x+"="+arr[x]);
+    } */
+   /* let y=2;
+   let res=true;
+   const mark=[60,60,59,60];
+   for(let i=0;i<mark.length;i++)
+    {
+        if(!passed(marks[i]))
+            res=false;
+    } */
+/* let mark1=[5,6,7,8];
+let mark2=[12,34,45,56];
+let mark3=[11,22,33,44]; */
+let day1=2;
+let month1=5;
+let year1=2009;
+
+let day2=4;
+let month2=8;
+let year2=2008;
+numberofDys1=(2025-year1)*360+(8-month1)*30+(31-day1)
+numberofDys2=(2025-year2)*360+(8-month2)*30+(31-day2)
+if(numberofDys1>numberofDys2){
+    console.log("the first");
+}
+else{
+    console.log("the secand");
+}
+    
