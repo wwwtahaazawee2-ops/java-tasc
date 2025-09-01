9bcf2a0ed17337cac1ef534ce22c9c03a517a452
wwwtahaazawee2-ops <wwwtahaazawee2@gmail.com>
2025-09-01 17:11:01 +0300

    t

--- gs/main.js
+++ gs/main.js
@@ -36,9 +36,9 @@ console.log(x+"="+arr[x]);
 /* let mark1=[5,6,7,8];
 let mark2=[12,34,45,56];
 let mark3=[11,22,33,44]; */
-let day1=2;
+/* let day1=12;
 let month1=5;
-let year1=2009;
+let year1=1999;
 
 let day2=4;
 let month2=8;
@@ -46,9 +46,51 @@ let year2=2008;
 numberofDys1=(2025-year1)*360+(8-month1)*30+(31-day1)
 numberofDys2=(2025-year2)*360+(8-month2)*30+(31-day2)
 if(numberofDys1>numberofDys2){
-    console.log("the first");
+    console.log(numberofDys1);
 }
 else{
-    console.log("the secand");
+    console.log(numberofDys2);
+} */
+
+/* function isprime(num){
+for(let i=2;i<num;i++){
+    if(num%i===0){
+        return false
+    }
+حساب اعداد اولية
 }
-    
+return true
+}
+
+for(let i=1;i<=100;i++)
+{
+if(isprime(i))
+    console.log(i);
+
+}                     */  
+/* function fector(num){
+    let x=1;
+    for(let i =2;i<=num;i++){
+ x=x*i;
+    }
+   return x    
+}عدد عاملي
+console.log(fector(10)); */
+/* function prog(num){
+    let x=0;
+    for(let i=1;i<num;i++)
+    {
+        if(num%i==0){
+            x=x+i;
+        }
+       
+    }
+     return x===num
+}
+
+for(let i=1;i<=10000;i++){
+if(prog(i)){
+    console.log(i);
+}
+} */
+
